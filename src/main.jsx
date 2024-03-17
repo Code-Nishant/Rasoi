ea1ac83270bcd9581b670461f37833980f7d6b3d
Nishant Choudhary <nc7377931568@gmail.com>
2024-03-18 01:04:39 +0530

    inital structure files with redux halfly configure

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+
+//redux importing and config
+import store from "./store/store.js";
+import { Provider } from "react-redux";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+
+    {/* redux store warpping */}
+    <Provider store={store}>
+      <App />
+    </Provider>
+    
+  </React.StrictMode>
+);
