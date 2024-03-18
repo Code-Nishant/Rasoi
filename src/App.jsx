dfb7681a6635ea5d8b291dcf842a2fc3c0aaa5bb
Nishant Choudhary <nc7377931568@gmail.com>
2024-03-19 00:09:37 +0530

    adding redux functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
 import './App.css'
+import { useSelector, useDispatch } from 'react-redux'
+import { increment } from './store/counterSlice';
 
 function App() {
+  const dispatch= useDispatch();
+  const count=useSelector((state)=>{
+    return state.counter.value
+  })
+  console.log(count);
 
   return (
     <div className="App bg-info">
-      jasdkjfsajlkfjlkaj
+      COUNT- {count};
+      {/* <button onClick={()=> dispatch(increment())}></button> */}
+      <button className="btn btn-primary" type="submit" onClick={()=> dispatch(increment())}>Button</button>
     </div>
   )
 }
 
 export default App
+
+/*
+Arrow function
+syntax->   (para1,para2)=> _____
+            (state)
+*/
+
