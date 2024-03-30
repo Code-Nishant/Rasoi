47461c8aeb45993d7bd9c3a7670472ed99d00dfc
Mahesmati Maharana <107037135+Mahesmati@users.noreply.github.com>
2024-03-31 01:22:33 +0530

    mirror changes

--- src/Pages/Home/Banner/data.jsx
+++ src/Pages/Home/Banner/data.jsx
@@ -1,24 +1,24 @@
 export const dataItem=[
     {
-        title: "Vegan & Vegetarian",
+        title: "Vegetarian",
         link: "",
         class:"veg",
         imagePath:"https://www.eatingwell.com/thmb/ZrLL0M2E7J_XILp5DMxTcYL3MJ4=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/vegetarian-protein-bowl-51d0dcb83a084b3da7e5bd29e0beae37.jpg"
     },
     {
-        title: "Biscuits & Cookies",
+        title: "Biscuits",
         link: "",
         class:"biscuits",
         imagePath:"https://p7.hiclipart.com/preview/194/352/29/bundt-cake-bakery-cupcake-mousse-mold-a-big-plate-of-biscuits.jpg"
     },
     {
-        title: "Desserts & Sweets",
+        title: "Desserts",
         link: "",
         class:"sweet",
         imagePath:"https://amazingksa.com/wp-content/uploads/2023/09/00bca9d12cd7bf398f10cd14c420abcf.jpg"
     },
     {
-        title: "Salads",
+        title: "Salad",
         link: "",
         class:"salad",
         imagePath:"https://toneop.s3.ap-south-1.amazonaws.com/toneop_eats/food/Twisted-Greek-Salad-Bowl.png"
@@ -30,7 +30,7 @@ export const dataItem=[
         imagePath:"https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2023/09/Vegetable-Soup-main.jpg"
     },
     {
-        title: "Starter & Side Dishes",
+        title: "Starter",
         link: "",
         class:"starter",
         imagePath:"https://w7.pngwing.com/pngs/592/778/png-transparent-pizza-on-white-plate-california-style-pizza-sicilian-pizza-italian-cuisine-tarte-flambee-pizza-food-pizza-logo-pizza-delivery.png"
@@ -42,7 +42,7 @@ export const dataItem=[
         imagePath:"https://shop.nutrifoodz.co/cdn/shop/products/PANCAKE-3_jpg_1_1000x1000_crop_center.jpg?v=1670242178"
     },
     {
-        title: "Ice Cream",
+        title: "Ice-Cream",
         link: "",
         class:"ice-cream",
         imagePath:"https://media.istockphoto.com/id/178510129/photo/plate-of-assorted-scooped-ice-cream-and-fresh-ingredients.jpg?s=612x612&w=0&k=20&c=YM4--hkXAjiyJ0r-IzHHXruTNWdZ2WMAsbAIAcAW98E="
