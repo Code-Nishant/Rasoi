bcde50b1057679cdb60117eb9222b23e47a4d6ed
Nishant Choudhary <nc7377931568@gmail.com>
2024-04-01 03:33:34 +0530

    explore cuisine added in home tab

--- src/Pages/Home/ExploreCuisine/ExploreCuisine.jsx
+++ src/Pages/Home/ExploreCuisine/ExploreCuisine.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import "./ExploreCuisine.scss"
+import image from "./p.svg";
 
 function ExploreCuisine() {
   return (
-    <div className='exploreCuisine'>ExploreCuisine</div>
-  )
+    <div className="exploreCuisine container-fluid">
+      <div className="inner">
+        
+
+        <div className="main">
+          <div className="title">
+            <h3>Cuisine</h3>
+          </div>
+          <div className="decription">
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod
+              aperiam nulla, error et natus nisi ipsam laboriosam veritatis,
+              quos at consequatur sequi expedita facilis omnis!
+            </p>
+          </div>
+          <div className="btn">
+            <button className="button">Explore Now</button>
+          </div>
+        </div>
+
+        <div className="image">
+          <img src={image} alt="image" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default ExploreCuisine
+export default ExploreCuisine;
