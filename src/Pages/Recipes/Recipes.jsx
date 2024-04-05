a663c9929ac0884abff7da612e4a6e5ee9792767
Mahesmati Maharana <107037135+Mahesmati@users.noreply.github.com>
2024-04-05 21:25:08 +0530

    added gallery on recipe's page

--- src/Pages/Recipes/Recipes.jsx
+++ src/Pages/Recipes/Recipes.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Card_one from "../../component/Cards/Carsousel/Card_one.jsx";
+import Card from "../../component/Cards/Card.jsx";
+import Gallery from "./Gallery/Gallery.jsx";
 
 import "./Recipes.scss";
 
@@ -9,8 +11,30 @@ function Recipes() {
   return (
     <div className="recipes">
       <TopBanner className="container-fluid" />
-      <h5>trending food recipe</h5>
+      <hr />
+      <h5 className="mt-6 text-center main heading">Explore Recipes</h5>
+      <hr />
       <Card_one />
+
+      <hr />
+      <h5 className="mt-6 text-center main heading">Explore Recipes</h5>
+      <hr />
+
+      <Gallery />
+      <hr />
+      <h5 className="mt-6 text-center main heading">Explore Recipes</h5>
+      <hr />
+      <Card_one />
+      <hr />
+      <h5 className="mt-6 text-center main heading">Explore Recipes</h5>
+      <hr />
+      <Card_one />
+      <hr />
+      <h5 className="mt-6 text-center main heading">Explore Recipes</h5>
+      <hr />
+      <Card_one />
+
+      {/* <Card /> */}
     </div>
   );
 }
