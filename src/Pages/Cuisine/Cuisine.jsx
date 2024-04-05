777df43c8fc7ad3d0712dab56458d5e29a264b57
Mahesmati Maharana <107037135+Mahesmati@users.noreply.github.com>
2024-04-06 03:04:54 +0530

    fixsation on cuisine

--- src/Pages/Cuisine/Cuisine.jsx
+++ src/Pages/Cuisine/Cuisine.jsx
@@ -1,13 +1,34 @@
 import React from "react";
 import SwiperAutoplay from "./SwiperAutoplay/SwiperAutoplay.jsx";
 import Modal from "../../component/Modal/Modal.jsx";
+import { BsArrowLeftShort } from "react-icons/bs";
+import { BsArrowRightShort } from "react-icons/bs";
 import "./Cuisine.scss";
-function Cuisine() {
+const Cuisine = () => {
   return (
-    <div className="cuisine">
-      <SwiperAutoplay />
-      {/* <Modal /> */}
+   <section className="cuisine">
+    <SwiperAutoplay />
+    <div className="secContainer">
+      <div className="secHeader">
+        <div className="textDiv">
+          <h2 className="secTitle">Explore Cuisines
+          </h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus ut nesciunt dolore? Harum obcaecati quia commodi voluptatem, ipsa quidem nesciunt temporibus eos illum minima, vero ipsum alias blanditiis veniam repellendus?
+
+          </p>
+        </div>
+        <div className="iconsDiv">
+        <BsArrowLeftShort className="icon" />
+        <BsArrowRightShort className="icon" />
+
+        </div>
+      </div>
+      <div className="mainContent">
+
+      </div>
     </div>
+
+   </section>
   );
 }
 
