314786c76d64d6a29a87fbadcc52b9e74ea971f9
Mahesmati Maharana <107037135+Mahesmati@users.noreply.github.com>
2024-04-09 03:48:01 +0530

    responsive in cuisine page

--- src/Pages/Cuisine/Cuisine.jsx
+++ src/Pages/Cuisine/Cuisine.jsx
@@ -2,6 +2,14 @@ import React from "react";
 import SwiperAutoplay from "./SwiperAutoplay/SwiperAutoplay.jsx";
 import Modal from "../../component/Modal/Modal.jsx";
 import Card_one from "../../component/Cards/Carsousel/Card_one.jsx";
+import { FaBowlRice } from "react-icons/fa6";
+import { IoFastFood } from "react-icons/io5";
+import { FaBowlFood } from "react-icons/fa6";
+import { GiNoodles } from "react-icons/gi";
+import { TbSalad } from "react-icons/tb";
+import { LuSalad } from "react-icons/lu";
+import { CiBowlNoodles } from "react-icons/ci";
+import { GiBowlOfRice } from "react-icons/gi";
 
 import "./Cuisine.scss";
 
@@ -11,39 +19,73 @@ const Cuisine = () => {
       <SwiperAutoplay />
       <section className="main">
         <hr />
-        <h5 className="mt-6 text-left main heading">Indian Cuisine</h5>
+        <div className="item1">
+          <h5 className="mt-6 text-left main heading">Indian Cuisine</h5>
+          <FaBowlRice />
+        </div>
         <hr />
+        <Card_one />
 
+        <hr />
+        <div className="item2">
+          <h5 className="mt-6 text-left main heading">American Cuisine</h5>
+          <IoFastFood />
+        </div>
+        <hr />
+        <Card_one />
+
+
+        <hr />
+       <div className="item3">
+       <h5 className="mt-6 text-left main heading">Asian Cuisine</h5>
+       <FaBowlFood />
+       </div>
+        <hr />
         <Card_one />
 
         <hr />
-        <h5 className="mt-6 text-left main heading">American Cuisine</h5>
+        <div className="item4">
+          <h5 className="mt-6 text-left main heading">Chinese Cuisine</h5>
+          <GiNoodles />
+        </div>
         <hr />
         <Card_one />
+
         <hr />
-        <h5 className="mt-6 text-left main heading">Asian Cuisine</h5>
-        <hr />
-        <Card_one />
-        <hr />
-        <h5 className="mt-6 text-left main heading">Chinese Cuisine</h5>
-        <hr />
-        <Card_one />
-        <hr />
+        <div className="item5">
         <h5 className="mt-6 text-left main heading">French Cuisine</h5>
+        <LuSalad />
+        </div>
         <hr />
         <Card_one />
+
+      
         <hr />
+        <div className="item6">
         <h5 className="mt-6 text-left main heading">Italian Cuisine</h5>
+        <TbSalad />
+        </div>
         <hr />
         <Card_one />
+
+
         <hr />
+        <div className="item7">
         <h5 className="mt-6 text-left main heading">Japanese Cuisine</h5>
+        <CiBowlNoodles />
+        </div>
         <hr />
         <Card_one />
+
+
         <hr />
+        <div className="item8">
         <h5 className="mt-6 text-left main heading">Mexican Cuisine</h5>
+        <GiBowlOfRice />
+        </div>
         <hr />
         <Card_one />
+
       </section>
     </div>
   );
--- src/component/Cards/Carsousel/Card_one.jsx
+++ src/component/Cards/Carsousel/Card_one.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { data } from "./data";
 import "./Card_one.scss";
-import ShowModal from "../../Modal/ShowModal.jsx"
+import ShowModal from "../../Modal/ShowModal.jsx";
 import Modal from "../../Modal/Modal.jsx";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
@@ -10,9 +10,69 @@ import { useDispatch, useSelector } from "react-redux";
 import { show } from "../../../store/modalSlice.js";
 
 function CardOne() {
+  const val = useSelector((state) => state.modal);
+  const dispatch = useDispatch();
 
-  const val= useSelector(state=> state.modal);//incomming of state from redux store directly
-  const dispatch=useDispatch();//outgoing of state from redux store directly
+  // let setSlidesToShow;
+
+  // const [width, setWidth] = useState(window.innerWidth);
+
+  // useEffect(() => {
+  //   const handleResize = () => {
+  //     if (width < 426) {
+  //       setSlidesToShow = 1;
+  //     } else if (width < 768) {
+  //       setSlidesToShow = 3;
+  //     } else if (width < 1024) {
+  //       setSlidesToShow = 4;
+  //     } else {
+  //       setSlidesToShow = 5;
+  //     }
+  //   };
+
+  //   
+
+  //   handleResize();
+
+  //   return () => {
+  //     window.removeEventListener("resize", handleResize);
+  //   };
+  // }, [width]);
+  // console.log(setSlidesToShow);
+
+  const [width, setWidth] = useState(window.innerWidth);
+  const [slide,setSlide] =  useState(1);
+  console.log (width);
+  useEffect (() => {
+
+
+
+    const  updateWindowDimensions = () => {
+      setWidth(window.innerWidth);
+
+      if(width<426)
+      {
+        setSlide(1);
+      }
+      else if (width < 769)
+      {
+        setSlide(3);
+      }
+      else if (width < 1025)
+      {
+        setSlide(4);
+      }
+      else 
+      {
+        setSlide(5);
+      }
+console.log(width+" "+ slide);
+    }
+    window.addEventListener("resize",updateWindowDimensions);
+
+  },[])
+
+
 
   const settings = {
     infinite: true,
@@ -24,21 +84,23 @@ function CardOne() {
   return (
     <div className="cardOne container">
       <Slider {...settings} className="slider">
-        {data.map((d,key) => (
+        {data.map((d, key) => (
           <div key={key} className="item rounded-xl">
             <div className="layer rounded-t-xl">
-              <img src={d.img} alt=""/>
+              <img src={d.img} alt="" />
             </div>
 
             <div className="description">
               <h4 className="text">{d.name}</h4>
               <p>{d.description}</p>
-              <button className='bttn' onClick={()=>dispatch(show())}>Read More</button>
+              <button className="bttn" onClick={() => dispatch(show())}>
+                Read More
+              </button>
             </div>
           </div>
         ))}
       </Slider>
-        {val.value && <ShowModal />}
+      {val.value && <ShowModal />}
     </div>
   );
 }
