48ec4c358304b1142aa5eed8aa1454c0f62899d6
Nishant Choudhary <nc7377931568@gmail.com>
2024-04-14 17:49:39 +0530

    added stickers for the project

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import "./About.scss";
+import img from "../../assets/Image/Contact/1-removebg-preview.png"
 
 function About() {
-  return <div>About</div>;
+  return <div className="about">
+    <img src={img} alt="" />
+  </div>;
 }
 
 export default About;
